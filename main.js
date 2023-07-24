c50fe01cf59a6cd505fbf4e3de8866b4d4af34c0
Gabriel9305 <134164145+Gabriel9305@users.noreply.github.com>
2023-07-24 16:16:28 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,66 +1,27 @@
-const form = document.getElementById('form-atividade');
-const imgAprovado = '<img src="./images/aprovado.png" alt="Emoji celebrando" />';
-const imgReprovado = '<img src="./images/reprovado.png" alt="Emoji decepcionado" />';
-const atividades = [];
-const notas = [];
-const spanAprovado = '<span class="resultado aprovado">Aprovado</span>';
-const spanReprovado = '<span class="resultado reprovado">reprovado</span>';
-const notaMinima = parseFloat(prompt("Digite a nota mínima"));
+$(document).ready(function() {
+    $('header button').click(function() {
+        $('form').slideDown();
+    })
 
-let linhas = '';
+    $('#botao-cancelar').click(function() {
+        $('form').slideUp();
+    })
 
-form.addEventListener('submit',function(e) {
-    e.preventDefault();
+    $('form').on('submit', function(e) {
+        e.preventDefault();
+        const enderecoDaNovaImagem = $('endereco-imagem-nova').val();
+        const novoItem = $(<li></li>);
+        $(`<img src="$(enderecoDaNovaImagem)" />`).appendTo(novoItem);
+        $(`
+            <div class="overlay-imagem-link">
+                <a href="$(enderecoNovaImagem)" target"_blank" title_"Ver imagem em tamanho real
+                    Ver imagem em tamanho real
+                </a>
+                </div>    
+            `).appendTo(novoItem);
+            $(novoItem).appendTo('ul');
 
-    adicionaLinha();
-    atualizaTabela();
-    atualizaMediaFinal();
-});
+    })
 
-function adicionaLinha() {
-    const inputNomeAtividade = document.getElementById('nome-atividade');
-    const inputNotaAtividade = document.getElementById('nota-atividade');
+})
 
-    if (atividades.includes(inputNomeAtividade.value)) {
-        alert(`A atividade: ${inputNomeAtividade.value} já foi inserida`);
-    } else {
-        atividades.push(inputNomeAtividade.value);
-        notas.push(parseFloat(inputNotaAtividade.value));
-    
-        let linha = '<tr>';
-        linha += `<td>${inputNomeAtividade.value}</td>`;
-        linha += `<td>${inputNotaAtividade.value}</td>`;
-        linha += `<td>${inputNotaAtividade.value >= notaMinima ? imgAprovado : imgReprovado}</td>`;
-        linha += '</tr>';
-    
-        linhas += linha; 
-    }
-
-    
-
-    inputNomeAtividade.value = '';
-    inputNotaAtividade.value = '';
-}
-
-function atualizaTabela() {
-    const corpoTabela = document.querySelector('tbody');
-    corpoTabela.innerHTML = linhas;
-
-}
-
-function atualizaMediaFinal() {
-    const mediaFinal = calculaMediaFinal();
-
-    document.getElementById('media-final-valor').innerHTML = mediaFinal.toFixed(2);
-    document.getElementById('media-final-resultado').innerHTML = mediaFinal >= notaMinima ? spanAprovado : spanReprovado;
-}
-
-function calculaMediaFinal() {
-    let somaDasNotas = 0;
-
-    for (let i = 0; i < notas.length; i++) {
-        somaDasNotas += notas[i];
-    }
-    
-    return somaDasNotas / notas.length;
-}
